a06b9eedd06e72b5678063f7fd37639c401f8828
KimTNguyen <kimthy1234@yahoo.com>
2016-07-13 00:46:43 -0700

    Add Java Doc Comments

--- src/santarosa/edu/homework/TestPayroll.java
+++ src/santarosa/edu/homework/TestPayroll.java
@@ -1,3 +1,11 @@
+/** 
+ * TestPayroll is a main class which tests the payroll system by using payroll object to calculate wage for multiple Employee objects.
+ * 
+ * @author	Kim Nguyen
+ * @version	%I%, %G%
+ * @since 	1.0
+ */
+
 package santarosa.edu.homework;
 
 import java.util.ArrayList;
@@ -10,6 +18,8 @@ public class TestPayroll {
 		double[] baseRate = { 7.5, 8.2, 10, 15, 10 };
 		int[] hours = { 35, 47, 73, 55, 50 };
 
+		// generates multiple Employee objects and assigns values for their name
+		// fields
 		for (int i = 1; i <= noEmployees; ++i) {
 			employeeList.add(new Employee());
 			employeeList.get(i - 1).setName(Integer.toString(i));
@@ -17,6 +27,7 @@ public class TestPayroll {
 
 		Payroll payroll = new Payroll();
 
+		// performs wage calculation for each employee objects
 		for (Employee employee : employeeList) {
 			payroll.calculate(employee, baseRate[employeeList.indexOf(employee)],
 					hours[employeeList.indexOf(employee)]);
